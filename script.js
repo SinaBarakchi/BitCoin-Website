69d507facb9f6b303171c4ad0de03a24969ada53
ProfessorSid <sinabarakchi@gmail.com>
2022-06-09 16:25:34 +0430

    bitcoin project

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMenu(){
+    var menu = document.getElementById("menu");
+    if(menu.className==="nav_list")
+        menu.className += " show";
+     else
+        menu.className =  "nav_list";   
+}
+// function close(){
+//     if(screen.width < 789)
+
+// }
